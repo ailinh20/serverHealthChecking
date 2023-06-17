332a0cd7f79f4abe04bc93bacc385a2638020319
ailinh20 <ailinhphan@icloud.com>
2023-06-17 12:49:16 +0700

    add age

--- models/model.js
+++ models/model.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const infoSensor = new mongoose.Schema({
+    age: {
+        type: Number
+    },
     heartbeat: {
-        required: true,
         type: Number
     },
     sp02: {
-        required: true,
         type: Number
     },
     timing: {
-        required: true,
         type: String
     }
 })
