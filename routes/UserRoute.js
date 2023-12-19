4503a461650442217016cd7aaf03ca05d693f650
ailinh20 <ailinhphan@icloud.com>
2023-12-20 03:53:03 +0700

    add api update user

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
     createUser,
     getAllUser,
-    getUser
+    getUser,
+    updateUser
 } = require("../controllers/UserController")
 
 router.route("/").get(getAllUser);
 router.route("/createuser").post(createUser);
-router.route("/getuser/:id").get(getUser)
+router.route("/getuser/:id").get(getUser);
+router.route("/updateuser/:id").put(updateUser);
 
 module.exports = router;
